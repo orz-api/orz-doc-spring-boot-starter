c9fe9ad0d6ce7cd2bad386805186441e6df042e5
reset7523 <reset7523@gmail.com>
2023-11-26 17:24:08 +0800

    Dev: improve codes

--- src/main/java/orz/springboot/doc/OrzDocConfiguration.java
+++ src/main/java/orz/springboot/doc/OrzDocConfiguration.java
@@ -2,15 +2,11 @@ package orz.springboot.doc;
 
 import org.springdoc.core.configuration.SpringDocConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.springdoc.core.utils.Constants.SPRINGDOC_ENABLED;
-
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureBefore(SpringDocConfiguration.class)
-@ConditionalOnProperty(name = SPRINGDOC_ENABLED, matchIfMissing = false)
 public class OrzDocConfiguration {
     @Bean
     public OrzDocParameterCustomizer parameterCustomizer(OrzDocSchemaConverter schemaConverter) {
